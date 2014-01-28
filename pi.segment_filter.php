6b980719201aec04cc3072446e070d52869a3584
speder <speder@pacbell.net>
2014-01-27 21:43:21 -0800

    Update pi.segment_filter.php
    Added debug

--- pi.segment_filter.php
+++ pi.segment_filter.php
@@ -1,24 +1,32 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 $plugin_info = array(
-  'pi_name'        => 'Segment Filter',
-  'pi_version'     => '1.0.0',
-  'pi_author'      => 'Steve Pedersen',
-  'pi_author_url'  => 'http://www.bluecoastweb.com',
-  'pi_description' => 'Filter extra segments',
-  'pi_usage'       => Segment_filter::usage()
+    'pi_name'        => 'Segment Filter',
+    'pi_version'     => '1.0.1',
+    'pi_author'      => 'Steve Pedersen',
+    'pi_author_url'  => 'http://www.bluecoastweb.com',
+    'pi_description' => 'Filter extra segments',
+    'pi_usage'       => Segment_filter::usage()
 );
 
 class Segment_filter {
+    private $debug = false;
 
     public function __construct() {
         $this->EE =& get_instance();
+        $this->debug = $this->is_truthy($this->EE->TMPL->fetch_param('debug'));
         $number = $this->EE->TMPL->fetch_param('number', 1);
         $segment = $this->EE->uri->segment($number);
+        if ($this->debug) {
+            $this->EE->TMPL->log_item(__CLASS__." number=$number, segment=$segment");
+        }
         if (! $segment) {
             return;
         }
         $permitted_values = array_filter(explode('|', $this->EE->TMPL->fetch_param('permitted', '')));
+        if ($this->debug) {
+            $this->EE->TMPL->log_item(__CLASS__.' permitted_values='.print_r($permitted_values, true));
+        }
         if (in_array($segment, $permitted_values)) {
             return;
         };
@@ -30,9 +38,17 @@ class Segment_filter {
                 $redirect .= implode('/', array_slice($segments, 0, $number - 1));
             }
         }
+        if ($this->debug) {
+            $this->EE->TMPL->log_item(__CLASS__." redirect=$redirect");
+        }
         header('HTTP/1.1 301 Moved Permanently');
         header("Location: $redirect");
         exit();
+    }
+
+    private function is_truthy($value) {
+        $truthy_values = array('on', 'true', 'yes', '1');
+        return in_array(strtolower($value), $truthy_values);
     }
 
     public static function usage() {
@@ -54,10 +70,10 @@ Or redirect to /segment_1/segment_2 (the default) if segment_3 is not within the
     {exp:segment_filter number='3' permitted='apples|oranges|mangoes'}
 
 <?php
-    $buffer = ob_get_contents();
-    ob_end_clean();
-    return $buffer;
-  }
+        $buffer = ob_get_contents();
+        ob_end_clean();
+        return $buffer;
+    }
 }
 
 /* End of file pi.segment_filter.php */
